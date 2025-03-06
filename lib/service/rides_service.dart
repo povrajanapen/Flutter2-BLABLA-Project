3b49f17748ac10ee12d2683ed2e52a8d3552979e
povrajanapen <povrajanapen@gmail.com>
2025-03-06 22:53:43 +0700

    BLA-102 – Refactor Rides Service

--- lib/service/rides_service.dart
+++ lib/service/rides_service.dart
@@ -1,4 +1,6 @@
 import 'package:week_3_blabla_project/model/ride_pref/ride_pref.dart';
+import 'package:week_3_blabla_project/repository/locations_repository.dart';
+import 'package:week_3_blabla_project/repository/mock/mock_rides_repository.dart';
 import 'package:week_3_blabla_project/repository/rides_repository.dart';
 import '../dummy_data/dummy_data.dart';
 import '../model/ride/ride.dart';
@@ -20,29 +22,28 @@ class RidesService {
     return availableRides.where( (ride) => ride.departureLocation == preferences.departure && ride.arrivalLocation == preferences.arrival).toList();
   }
 
-   /// - singleton instance
-  static final RidesService _instance = RidesService._internal();
-  static late RidesRepository _repository;
+  // - ride service single pattern
   
+  static RidesService? _instance;
+  final RidesRepository repository;
 
+  RidesService._internal(this.repository);
 
-  /// - private constructor, to prevent creating new instance from outside
-  RidesService._internal();
-
-  // - factory constructor, to return the same instance everytime
-  factory RidesService(){
-    return _instance;
-  }
 
   /// - initialize the rides list that will be used in the service
-  
   static void initialize(RidesRepository repository){
-    _repository = repository;
+    _instance = RidesService._internal(repository);
   }
+
+  static RidesService get instance{
+    _instance ??= RidesService._internal(MockRidesRepository());
+    return _instance!;
+  }
+
 
   /// - returns a list of rides based on passenger preferences and optional filters
   List<Ride> getRides(RidePreference preference, RidesFilter? filter) {
-    return _repository.getRides(preference, filter);
+    return repository.getRides(preference, filter);
   }
 }
 
